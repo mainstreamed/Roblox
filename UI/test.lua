912061f737cb29df53f7197b79fdcac3b9ad70a9
Mainstreamed <106301048+Mainstreamed@users.noreply.github.com>
2023-10-08 19:04:12 +1000

    Update test.lua

--- UI/test.lua
+++ UI/test.lua
@@ -1,21 +1,26 @@
 return {
-  { 
+  {
+    key = 'Zsn2qNbtBDzMDULvPw9Mjhdly1ApI0Nf',
     ip = '64.160.231.192',
     hwid = 'c2f52047-9260-4aed-b82b-b87b86442d24'
   },
   { 
+    key = 'fszrq4ljYHhnwMK7Ac6zqBJHn0VrsWn8',
     ip = '5.65.255.187',
     hwid = 'f5ae7927-d350-4e12-88fa-b48aa4d76885'
   },
   { 
+    key = 'zSKDoMOTpuEvckYfhc7VHv6imrxZ2wAG',
     ip = '59.135.162.82',
     hwid = 'e14a8b9f-0e29-4b58-a35c-b8eb995327c1'
   },
   { 
+    key = 'thb7PvTXGcwohnWxyeooGqtvoaiGwsH9',
     ip = '5.38.144.180',
     hwid = '9174d95e-dded-45bf-a5c6-f754a0865493'
   },
   { 
+    key = 'hhGwR1UCk2dSp2bqU8J7UQQnDmHLuFg1',
     ip = '223.173.137.16',
     hwid = '19d373b1-2348-4525-8fa9-10dac8c6d1ee'
   },
