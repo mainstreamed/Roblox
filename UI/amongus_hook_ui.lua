bbcc62405fd84f4c3a462d3c622b505ce917cd4a
Mainstreamed <106301048+Mainstreamed@users.noreply.github.com>
2024-02-16 18:42:52 +1000

    Update amongus_hook_ui.lua

--- UI/amongus_hook_ui.lua
+++ UI/amongus_hook_ui.lua
@@ -6,33 +6,16 @@ local vector2_new = Vector2.new;
 local color3_new = Color3.new;
 local color3_fromrgb = Color3.fromRGB;
 local cframe_new = CFrame.new;
-local userinputservice = getservice(game, 'UserInputService');
 local instance_new = Instance.new;
 local math_huge = math.huge;
 local table_insert = table.insert;
 
+local userinputservice = getservice(game, 'UserInputService');
 local camera = workspace.CurrentCamera
-local screensize = camera.ViewportSize
-local globalwidth = screensize.X / 18
-
-local library = {
-	tabamount = 0,
-	tabselected = 1,
-    tabopened = false,
-	inputfunctions = {
-		Up = {},
-		Down = {},
-		Right = {},
-		Left = {},
-		Return = {},
-		Backspace = {},
-	},
-	tabs = {},
-	alldrawings = {},
-};
+local menuwidth = camera.ViewportSize.X / 18
 
 local function createDrawing(type, properties, add)
-	local drawing = drawing_new(type)
+	local drawing = drawing_new(type);
 	if (properties) then
 		for index, value in properties do
 			drawing[index] = value;
@@ -46,249 +29,324 @@ local function createDrawing(type, properties, add)
 	return drawing;
 end
 
--- init
+-- main library
+local library = {
+    inputs = { -- all functions used for inputs (to save connections)
+		Up = {},
+		Down = {},
+		Right = {},
+		Left = {},
+		Return = {},
+		Backspace = {},
+	},
+    tabinfo = { -- all tab data here
+        active = false,
+        amount = 0,
+        selected = 1,
+        tabs = {},
+    },
+    alldrawings = {}, -- all drawings get stored in here
+}
+
+-- library functions
 do
-    userinputservice.InputBegan:Connect(function(key)
-        local funcs = library.inputfunctions[key.KeyCode.Name];
-        if (funcs) then
-            for index, value in funcs do
-                value()
-            end
+	-- add arrow input function
+	function library:dInput(key, func)
+		table_insert(library.inputs[key], func);
+	end
+    function library:Unload()
+        for _, value in library.alldrawings do
+            value:Remove();
         end
-    end)
-    table_insert(library.inputfunctions.Up, function()
-        if (not library.tabopened and library.tabselected > 1) then
-            library.tabs[library.tabselected]:Toggle()
-            library.tabselected-=1
-            library.tabs[library.tabselected]:Toggle()
-        end
-    end)
-    table_insert(library.inputfunctions.Down, function()
-        if (not library.tabopened and library.tabselected < library.tabamount) then
-            library.tabs[library.tabselected]:Toggle()
-            library.tabselected+=1
-            library.tabs[library.tabselected]:Toggle()
-        end
-    end)
+        library.mainconnection:Disconect();
+        library = nil;
+    end
 end
 
-function library:AddTab(name)
-	local selected = false;
-	if (library.tabamount == 0) then
-		selected = true;
-	end
-	library.tabamount += 1;
-	local tab = {
-		selected = selected,
-		opened = false,
-        selected_option = 1,
-		drawings = {},
-        option = {},
-		options = 0,
-	}
-	-- drawings
-	do
-		tab.drawings.base = createDrawing('Square', {
-			Visible = true,
-			Transparency = 0.5,
-			Filled = true,
-			Position = vector2_new(0, 40 + (library.tabamount * 15)),
-			Size = vector2_new(globalwidth, 15),
-		}, {library.alldrawings})
-		tab.drawings.textmain = createDrawing('Text', {
-			Color = color3_new(1, 1, 1),
-			Visible = true,
-			Font = 2,
-			Position = tab.drawings.base.Position,
-			Size = 13,
-			Text = name,
-		}, {library.alldrawings})
-		tab.drawings.arrow = createDrawing('Text', {
-			Color = color3_new(1, 1, 1),
-			Visible = true,
-			Text = '>',
-			Font = 2,
-			Position = tab.drawings.base.Position + vector2_new(globalwidth-10, 0),
-			Size = 13,
-		}, {library.alldrawings})
-	end
-	-- functions
-	do
-		function tab:AddToggle(properties)
-			tab.options += 1;
-			local toggle = {
-                hovered = false,
-				enabled = properties.default or false,
-				drawings = {},
-			}
-			-- drawings
-			do
-				toggle.drawings.base = createDrawing('Square', {
-					Transparency = 0.5,
-					Filled = true,
-					Position = tab.drawings.base.Position + vector2_new(globalwidth + 10, 0),
-					Size = vector2_new(globalwidth, 15),
-				}, {library.alldrawings})
-				toggle.drawings.textmain = createDrawing('Text', {
-					Color = color3_new(1, 1, 1),
-					Visible = true,
-					Font = 2,
-					Position = toggle.drawings.base.Position,
-					Size = 13,
-					Text = properties.text or 'Toggle',
-				}, {library.alldrawings})
-			end
-			-- functions
-			do
-				toggle.toggle = function(boolean)
-                    if (toggle.hovered) then
-                        if (boolean == nil) then
-                            boolean = not toggle.enabled;
-                        end
-                        toggle.enabled = boolean
-                        if (boolean) then
-                            toggle.drawings.textmain.Color = color3_new(1, 1, 1); 
-                        else
-                            toggle.drawing.textmain.Color = color3_fromrgb(79, 79, 79);
-                        end
-                    end
-				end
-			end
-            -- functionality
-            do
-                table_insert(library.inputfunctions.Return, toggle.toggle)
-                if (toggle.enabled) then
-                    toggle.drawings.textmain.Color = color3_new(1, 1, 1); 
-                else
-                    toggle.drawing.textmain.Color = color3_fromrgb(79, 79, 79);
-                end
+-- initialise
+do
+	-- detecting inputs (reducing connections)
+    library.mainconnection = userinputservice.InputBegan:Connect(function(key)
+        local funcs = library.inputs[key.KeyCode.Name];
+        if (funcs) then
+            for _, func in funcs do
+                func();
             end
-            table_insert(tab.option, toggle)
+        end
+    end)
+	-- inputs for going up and down the tabs
+	library:dInput('Up', function()
+		local ti = library.tabinfo;
+		if (not ti.active and ti.selected > 1) then
+			ti.tabs[ti.selected]:hovered();
+            ti.selected-=1;
+            ti.tabs[ti.selected]:hovered();
 		end
-		function tab:AddSlider(properties)
-			tab.options += 1;
-			local slider = {
-				hovered = false,
-				value = properties.default or properties.min,
-				drawings = {},
-			}
-			-- drawings
-			do
-				slider.drawings.base = createDrawing('Square', {
-					Transparency = 0.5,
-					Filled = true,
-					Position = tab.drawings.base.Position + vector2_new(globalwidth + 10, 0),
-					Size = vector2_new(globalwidth, 15),
-				}, {library.alldrawings})
-				slider.drawings.textmain = createDrawing('Text', {
-					Color = color3_new(1, 1, 1),
-					Visible = true,
-					Font = 2,
-					Position = slider.drawings.base.Position,
-					Size = 13,
-					Text = (properties.text or 'Slider')..': '..slider.value,
-				}, {library.alldrawings})
-			end
-			-- functions
-			do
-				function slider:updateValue()
-					slider.drawings.textmain.Text = (properties.text or 'Slider')..': '..slider.value;
-				end
-				slider.increase = function()
-                    if (slider.hovered) then
-                        local val = slider.value + 1;
-                        if (val <= properties.max) then
-                            slider.value = val;
-                            slider:updateValue();
-                        end
-                    end
-				end
-				slider.decrease = function()
-                    if (slider.hovered) then
-                        local val = slider.value - 1;
-                        if (val >= properties.min) then
-                            slider.value = val;
-                            slider:updateValue();
-                        end
-                    end
-				end
-			end
-			-- hook
-			do
-                table_insert(library.inputfunctions.Right, slider.increase)
-                table_insert(library.inputfunctions.Left, slider.decrease)
-			end
-            table_insert(tab.option, slider)
+	end)
+	library:dInput('Down', function()
+		local ti = library.tabinfo;
+		if not (not ti.active and ti.selected > 1) then
+			ti.tabs[ti.selected]:hovered();
+            ti.selected+=1;
+            ti.tabs[ti.selected]:hovered();
 		end
-		function tab:Toggle(boolean)
-			if (boolean == nil) then
-				boolean = not tab.selected
-			end
-			if (boolean) then
-				tab.drawings.base.Color = color3_new(1, 0, 0);
-			else
-				tab.drawings.base.Color = color3_new(0, 0, 0);
-			end
-            tab.selected = boolean
+	end)
+	library:dInput('Return', function()
+		local ti = library.tabinfo;
+		if (ti.active) then
+			return;
 		end
-        tab.open = function()
-            if (not tab.opened) then
-                tab.drawings.arrow.Text = '>'
-                library.tabopened = true;
-                tab.opened = true;
-                for index, value in option do
-                    for index, value in value.drawings do
-                        value.Visible = true;
-                    end
-                end
-            end
-        end
-        tab.close = function()
-            if (tab.opened) then
-                tab.drawings.arrow.Text = '<'
-                library.tabopened = false;
-                tab.opened = false;
-                for index, value in option do
-                    for index, value in value.drawings do
-                        value.Visible = false;
-                    end
-                end
-            end
-        end
-        tab.navigateDown = function()
-            if (tab.opened and tab.selected_option < tab.options) then
-                local old = tab.option[tab.selected_option]
-                old.hovered = false
-                old.drawings.base.Color = color3_new(1, 0, 0);
-                tab.selected_option+=1
-                local new = tab.option[tab.selected_option]
-                new.hovered = true
-                new.drawings.base.Color = color3_new(0, 0, 0);
-            end
-        end
-        tab.navigateUp = function()
-            if (tab.opened and tab.selected_option > 1) then
-                local old = tab.option[tab.selected_option]
-                old.hovered = false
-                old.drawings.base.Color = color3_new(1, 0, 0);
-                tab.selected_option-=1
-                local new = tab.option[tab.selected_option]
-                new.hovered = true
-                new.drawings.base.Color = color3_new(0, 0, 0);
-            end
-        end
-	end
-	-- functionality
-	do
-		tab:Toggle(tab.selected)
+		ti.tabs[ti.selected]:open();
+	end)
+	library:dInput('Backspace', function()
+		local ti = library.tabinfo;
+		if (not ti.active) then
+			return;
+		end
+		ti.tabs[ti.selected]:close();
+	end)
+end
 
-        table_insert(library.inputfunctions.Return, tab.open)
-        table_insert(library.inputfunctions.Backspace, tab.close)
+-- user functions
+do
+	function library:AddTab(text)
+		-- tab startup
+		local hovered = false;
+		if (library.tabinfo.amount == 0) then
+			hovered = true;
+		end
+		library.tabinfo.amount += 1;
+		-- creating tab
+		local tab = {
+			hovered = false,
+			opened = false,
+			selected = 1,
+			drawings = {},
+			options = {
+				amount = 0,
+				stored = {},
+			},
+		}
+		-- creating drawings
+		do
+			tab.drawings.base = createDrawing('Square', {
+				Visible = true,
+				Color = color3_fromrgb(0, 0, 0),
+				Transparency = 0.5,
+				Filled = true,
+				Position = vector2_new(0, 40 + (library.tabinfo.amount * 15)),
+				Size = vector2_new(menuwidth, 15),
+			}, {library.alldrawings})
+			tab.drawings.text = createDrawing('Text', {
+				Visible = true,
+				Color = color3_fromrgb(255, 255, 255),
+				Font = 2,
+				Position = tab.drawings.base.Position,
+				Size = 13,
+				Text = text,
+			}, {library.alldrawings})
+			tab.drawings.arrow = createDrawing('Text', {
+				Visible = true,
+				Color = color3_fromrgb(255, 255, 255),
+				Text = '>',
+				Font = 2,
+				Position = tab.drawings.base.Position + vector2_new(menuwidth-10, 0),
+				Size = 13,
+			}, {library.alldrawings})
 
-        table_insert(library.inputfunctions.Up, tab.navigateUp)
-        table_insert(library.inputfunctions.Down, tab.navigateDown)
+		end
+		-- functions
+		do
+			function tab:hovered(boolean)
+				if (boolean == nil) then
+					boolean = not tab.opened;
+				end
+				tab.hovered = boolean;
+				if (boolean) then
+					tab.drawings.base.Color = color3_fromrgb(255, 0, 0);
+					return;
+				end
+				tab.drawings.base.Color = color3_fromrgb(0, 0, 0);
+			end
+			function tab:open()
+				if (tab.opened) then
+					return;
+				end
+				library.tabinfo.active = true;
+				tab.opened = true;
+				tab.drawings.arrow.Text = '>';
+                for _, option in tab.options do
+                    for _, drawing in option.drawings do
+                        drawing.Visible = true;
+                    end
+                end
+			end
+			function tab:close()
+				if (not tab.opened) then
+					return;
+				end
+				library.tabinfo.active = false;
+				tab.opened = false;
+				tab.drawings.arrow.Text = '<';
+                for _, option in tab.options do
+                    for _, drawing in option.drawings do
+                        drawing.Visible = false;
+                    end
+                end
+			end
+			tab.navUp = function()
+				if (tab.opened and tab.selected < tab.options.amount) then
+					local current = tab.options.stored[tab.selected];
+					current.hovered = false;
+					current.drawings.base.Color = color3_fromrgb(0, 0, 0);
+					tab.selected -= 1;
+					local current = tab.options.stored[tab.selected];
+					current.hovered = true;
+					current.drawings.base.Color = color3_fromrgb(255, 0, 0);
+				end
+			end
+			tab.navDown = function()
+				if (tab.opened and tab.selected > 1) then
+					local current = tab.options.stored[tab.selected];
+					current.hovered = false;
+					current.drawings.base.Color = color3_fromrgb(0, 0, 0);
+					tab.selected += 1;
+					local current = tab.options.stored[tab.selected];
+					current.hovered = true;
+					current.drawings.base.Color = color3_fromrgb(255, 0, 0);
+				end
+			end
+			function tab:AddToggle(prop)
+				tab.options.amount += 1;
+				local toggle = {
+					hovered = false,
+					enabled = prop.default or false,
+					drawings = {},
+				}
+
+				-- drawings
+				do
+					toggle.drawings.base = createDrawing('Square', {
+						Transparency = 0.5,
+						Filled = true,
+						Position = tab.drawings.base.Position + vector2_new(menuwidth + 10, tab.options.amount*15),
+						Size = vector2_new(menuwidth, 15),
+					}, {library.alldrawings})
+					toggle.drawings.text = createDrawing('Text', {
+						Color = color3_fromrgb(255, 255, 255),
+						Visible = true,
+						Font = 2,
+						Position = toggle.drawings.base.Position,
+						Size = 13,
+						Text = prop.text or 'Toggle',
+					}, {library.alldrawings})
+				end
+				--functions 
+				do
+					toggle.toggle = function(boolean)
+						if (not toggled.hovered) then
+							return;
+						end
+						if (boolean == nil) then
+							boolean = not toggle.enabled;
+						end
+						toggle.enabled = boolean;
+						if (boolean) then
+							toggle.drawings.text.Color = color3_fromrgb(255, 255, 255);
+							return; 
+						end
+						toggle.drawings.text.Color = color3_fromrgb(79, 79, 79);
+					end
+				end
+				-- functionality / cleanup
+				do
+					library:dInput('Return', toggle.toggle)
+					if (toggle.enabled) then
+						toggle.drawings.textmain.Color = color3_fromrgb(255, 255, 255); 
+					else
+						toggle.drawing.textmain.Color = color3_fromrgb(79, 79, 79);
+					end
+					if (tab.options.amount == 1) then
+						toggle.hovered = true;
+						toggle.drawings.base.Color = color3_fromrgb(255, 0, 0);
+					end
+
+				end
+				table_insert(tab.options.stored, toggle)
+			end
+			function tab:AddSlider(prop)
+				tab.options.amount += 1
+				local slider = {
+					hovered = false,
+					text = prop.text or 'Slider',
+					value = prop.default or prop.min,
+					drawings = {},
+				}
+				-- drawings
+				do
+					slider.drawings.base = createDrawing('Square', {
+						Transparency = 0.5,
+						Filled = true,
+						Position = tab.drawings.base.Position + vector2_new(menuwidth + 10, tab.options.amount*15),
+						Size = vector2_new(menuwidth, 15),
+					}, {library.alldrawings})
+					slider.drawings.text = createDrawing('Text', {
+						Color = color3_fromrgb(255, 255, 255),
+						Visible = true,
+						Font = 2,
+						Position = slider.drawings.base.Position,
+						Size = 13,
+						Text = slider.text..': '..slider.value,
+					}, {library.alldrawings})
+				end
+				--functions 
+				do
+					slider.updatetext = function()
+						slider.drawings.textmain.Text = slider..': '..slider.value;
+					end
+					slider.increase = function()
+						if (not slider.hovered) then
+							return;
+						end
+						local val = slider.value + 1;
+						if (val <= prop.max) then
+							slider.value = val;
+							slider.updatetext();
+						end
+					end
+					slider.decrease = function()
+						if (not slider.hovered) then
+							return;
+						end
+						local val = slider.value - 1;
+						if (val >= prop.min) then
+							slider.value = val;
+							slider.updatetext();
+						end
+					end
+				end
+				-- functionality / cleanup
+				do
+					library:dInput('Right', slider.increase);
+					library:dInput('Left', slider.decrease);
+
+					if (tab.options.amount == 1) then
+						toggle.hovered = true;
+						toggle.drawings.base.Color = color3_fromrgb(255, 0, 0);
+					end
+				end
+
+				table_insert(tab.options.stored, slider)
+			end
+		end
+		-- functionality / cleanup
+		do
+			tab:hovered(hovered);
+			dInput('Up', tab.navUp);
+			dInput('Down', tab.navDown);
+		end
+		table_insert(library.tabinfo.tabs, tab);
 	end
-	table_insert(library.tabs, tab)
 end
 
 return library;
